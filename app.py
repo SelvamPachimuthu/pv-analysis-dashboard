e018f5a804434ed921acfa1a062ac2f4fce0ef2f
git config --SelvamPachimuthu <selvampachimuthu98@gmail.com>
2026-03-31 12:40:31 +0530

    Added age grp

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
+import pandas as pd
 from queries import get_all_cases, get_serious_cases
-import plotly.express as px
 
 st.title("Pharmacovigilance Dashboard")
 
@@ -10,5 +10,26 @@ st.dataframe(df)
 st.subheader("Serious vs Non-Serious")
 serious_df = get_serious_cases()
 st.bar_chart(serious_df.set_index("seriousness"))
-fig = px.bar(serious_df, x="seriousness", y="count")
-st.plotly_chart(fig, use_container_width=True)
+
+# 🔥 Age Group Function
+def age_group(age):
+    if pd.isna(age):
+        return "Unknown"
+    elif age < 18:
+        return "Pediatric"
+    elif age <= 65:
+        return "Adult"
+    else:
+        return "Elderly"
+
+df["age_group"] = df["patient_age"].apply(age_group)
+
+age_df = df.groupby("age_group").size().reset_index(name="count")
+
+# Order properly
+age_df = age_df.set_index("age_group").reindex(
+    ["Pediatric", "Adult", "Elderly", "Unknown"]
+)
+
+st.subheader("Cases by Age Group")
+st.bar_chart(age_df)
