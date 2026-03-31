fc1a7b9654d27a2ec347300857ebc5aef01363bf
git config --SelvamPachimuthu <selvampachimuthu98@gmail.com>
2026-03-31 13:12:43 +0530

    Added

--- queries.py
+++ queries.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def get_all_cases():
-    return pd.read_csv("pv_argus_100_cases.csv")
+    return pd.read_csv("C:\Users\ADMIN\Downloads\Personal\PV_Analysis\pv_argus_100_cases.csv")
 
 def get_serious_cases():
     df = pd.read_csv("pv_argus_100_cases.csv")
