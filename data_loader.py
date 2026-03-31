4bfaf5206df738abc04b7034a5e189e80463f0dd
git config --SelvamPachimuthu <selvampachimuthu98@gmail.com>
2026-03-31 12:21:53 +0530

    data loader added

--- data_loader.py
+++ data_loader.py
@@ -1,15 +1,3 @@
-import pandas as pd
-from db_config import get_connection
-
-conn = get_connection()
-cursor = conn.cursor()
-
-df = pd.read_csv(r"C:\Users\ADMIN\Downloads\Personal\PV_Analysis\pv_argus_100_cases.csv")
-
+df = pd.read_csv(...)
 for _, row in df.iterrows():
-    cursor.execute("""
-        INSERT INTO cases VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-    """, tuple(row))
-
-conn.commit()
-conn.close()
+    cursor.execute("INSERT INTO cases VALUES (...)")
